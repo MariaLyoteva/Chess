fdd5b57e926d6c2de895967fa58bf0f7cee101dd
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2021-06-19 20:40:04 +0300

    Main File

---
+++ ChessPlayer.java
@@ -0,0 +1,16 @@
+package com.chess;
+
+import java.io.File;
+
+import com.chess.gui.Table;
+
+public class ChessPlayer {
+
+    public static void main(final String[] args) throws Exception {
+        Table.get().show();
+      
+        File file = new File("C:\\ChessProject\\BlackWidow-Chess-master\\art1");
+      //  System.out.println(" ------> " + file.canRead());
+      //  System.out.println(" ---filePath---> " + file.getAbsolutePath());
+        }
+}
